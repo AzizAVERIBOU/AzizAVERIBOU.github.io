7efecceee84ca34ccc90c9a42df36d89bfc56b87
AzizAVERIBOU <azizaveribou6123@gmail.com>
2025-02-04 22:32:21 -0500

    Add redirect configuration and index.html for GitHub Pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
     path('admin/', admin.site.urls),  # Route pour l'interface d'administration
     path('', include('main.urls')),   # Route principale qui inclut toutes les URLs de l'app 'main'
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
